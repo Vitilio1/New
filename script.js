90a2b4e05b6a2376b83c5a7a32fd6288d0900404
Vitilio1 <vick.covalev@yandex.ru>
2022-06-16 09:04:54 +0300

    N

---
+++ script.js
@@ -0,0 +1,37 @@
+let Todo=document.querySelector('#Tasks');
+let Todos=document.querySelector('#todo');
+let input=document.querySelector('#input-id');
+let btn=document.querySelector('#btn');
+//let Tasks=document.querySelector('Tasks');
+let htmlblocks=document.querySelector('#container');
+
+let Tasks=[];
+btn.addEventListener('click',function(){
+    Tasks.push([
+        Date.now(),//id
+        input.value,//text
+        false,//checked
+    ])
+    let div=document.createElement('div');
+    div.className='newRow';
+    div.innerHTML=`
+    <div class='Tasks'>
+    <ul id='todo'>`+input.value+`</ul>
+    <button id=delete></button>
+    </div>
+    `;
+    htmlblocks.appendChild(div);
+})
+
+
+//btn.onclick = function(){
+//    console.log(input.value);
+//}
+//btn.addEventListener('click',function(){
+//    let Todo={
+//        id:Date.now(),
+//        Text:input.value,
+//        checked:false,
+//    } 
+//}) 
+//Todos.push(Todo);
